db26771fa84abe407e4159ba8c99228bae4319dd
Rudransh Bhardwaj <aarti19830@gmail.com>
2023-12-10 15:43:23 +0530

    added more code fixing some issues , this commit is not stable

---
+++ build.sh
@@ -0,0 +1,12 @@
+# go to the src folder
+cd ./src
+
+# create scss to css file 
+npm run scss
+
+# create css to index.css 
+npm run next-gen-css
+
+#pause to see outpu
+echo "You can close this , the css file has been built successfully !"
+read -p "Press enter to continue"
